dbfe07944f98778296e6162c580c78d2bcbcd51e
Derrick <doublehelix9@gmail.com>
2020-06-24 23:31:32 +0800

    IndexMinPQ is now capped by capacity.

--- pqueue/index_priority.go
+++ pqueue/index_priority.go
@@ -1,6 +1,11 @@
 package pqueue
 
-//import "fmt"
+import "errors"
+
+var (
+	ErrNoMinKeyFound = errors.New("No min key found")
+	ErrIndexOutOfBound = errors.New("Index out of bound")
+)
 
 // IndexMinPQ is a variant of minimum-oriented
 // priority queue that is indexed
@@ -11,14 +16,66 @@ type IndexMinPQ struct {
 	size int
 }
 
+// New construct a new instance of IndexMinPQ
+func NewIndexMinPQ(capacity int) IndexMinPQ {
+	if capacity < 1 {
+		capacity = 20
+	}
+	// create an instance of IndexMinPQ by initializing pq, qp and keys
+	p := IndexMinPQ{
+		make([]int, capacity + 1),
+		make([]int, capacity),
+		make([]int, capacity), 0,
+	}
+	// assign all elements in qp to -1 indicating index not set
+	for i := 0; i < len(p.qp); i++ {
+		p.qp[i] = -1
+	}
+	return p
+}
+
+func (p *IndexMinPQ) Size() int {
+	return p.size
+}
+
 // Insert puts a new key at index i
-func (p *IndexMinPQ) Insert(i int, key int) {
+func (p *IndexMinPQ) Insert(i int, key int) error {
 	p.size++
-	p.pq = append(p.pq, i)
-	p.qp = insertInverse(p.qp, i, p.size)
+	if i >= 0 && i < len(p.keys) {
+		p.pq[p.size] = i
+		p.qp[i] = p.size
+		p.keys[i] = key
+		p.swim(p.size)
+		return nil
+	}
+	return ErrIndexOutOfBound
+	//p.qp = insertInverse(p.qp, i, p.size)
 	//fmt.Printf("p.qp %v\n", p.qp)
-	p.keys = insertKey(p.keys, i, key)
-	p.swim(p.size)
+	//p.keys = insertKey(p.keys, i, key)
+}
+
+func (p *IndexMinPQ) Contains(i int) bool {
+	return p.qp[i] != -1
+}
+
+func (p *IndexMinPQ) MinKey() (int, error) {
+	if p.size > 0 {
+		return p.keys[p.pq[1]], nil
+	}
+	return -1, ErrNoMinKeyFound
+}
+
+func (p *IndexMinPQ) DelMin() (int, error) {
+	if p.size > 0 {
+		i := p.pq[1]
+		p.qp[i] = -1
+		p.pq[1] = p.pq[p.size]
+		p.pq[p.size] = 0
+		p.sink(1)
+		p.size--
+		return i, nil
+	}
+	return -1, ErrNoMinKeyFound
 }
 
 func (p *IndexMinPQ) swim(k int) {
@@ -29,9 +86,24 @@ func (p *IndexMinPQ) swim(k int) {
 		// swim, otherwise we have restored the heap order
 		//fmt.Printf("p.keys[%d] == %d\n", p.pq[k], p.keys[p.pq[k]])
 		//fmt.Printf("p.keys[%d] == %d\n", p.pq[k/2], p.keys[p.pq[k/2]])
-		if p.keys[p.pq[k]] > p.keys[p.pq[k/2]] {
+		if p.keys[p.pq[k]] < p.keys[p.pq[k/2]] {
 			p.pq[k], p.pq[k/2] = p.pq[k/2], p.pq[k]
 			p.swim(k / 2)
+		}
+	}
+}
+
+func (p *IndexMinPQ) sink(k int) {
+	j := 2 * k
+	if j < p.size {
+		if j + 1 <= p.size {
+			if p.keys[p.pq[j-1]] < p.keys[p.pq[j]] {
+				j++
+			}
+		}
+		if p.keys[p.pq[j]] < p.keys[p.pq[k]] {
+			p.pq[k], p.pq[j] = p.pq[j], p.pq[k]
+			p.sink(j)
 		}
 	}
 }
--- pqueue/index_priority_test.go
+++ pqueue/index_priority_test.go
@@ -5,44 +5,142 @@ import (
 	//"fmt"
 )
 
-type idxMinPQtestCase struct {
-	in   [][2]int
-	want [3][]int
-}
+
+type tuple2 [2]int
 
 func TestIndexMinPQInsertion(t *testing.T) {
-	cases := []idxMinPQtestCase{
+	type testCase struct {
+		in []tuple2
+		want [3][]int
+	}
+	cases := []testCase{
 		{
-			[][2]int{{1, 1}, {2, 2}},
-			[3][]int{{2, 1}, {-1, 1, 2}, {0, 1, 2}},
+			[]tuple2{{1, 1}, {2, 2}},
+			[3][]int{
+				{0, 1, 2, 0, 0, 0, 0, 0, 0},
+				{-1, 1, 2, -1, -1, -1, -1, -1},
+				{0, 1, 2, 0, 0, 0, 0, 0},
+			},
 		},
 		{
-			[][2]int{{2, 7}, {4, 10}, {6, 13}, {8, 8}},
+			[]tuple2{{2, 7}, {4, 10}, {6, 13}, {7, 8}},
 			[3][]int{
-				{6, 8, 4, 2},
-				{-1, -1, 1, -1, 2, -1, 3, -1, 4, -1, -1, -1},
-				{0, 0, 7, 0, 10, 0, 13, 0, 8, 0, 0, 0},
+				{0, 2, 7, 6, 4, 0, 0, 0, 0},
+				{-1, -1, 1, -1, 2, -1, 3, 4},
+				{0, 0, 7, 0, 10, 0, 13, 8},
 			},
 		},
 	}
 	for _, c := range cases {
-		// create an instance of IndexMinPQ by initializing pq, qp and keys
-		p := IndexMinPQ{make([]int, 0, 3), make([]int, 3), make([]int, 3), 0}
-		// first element of pq is not used, so fill it away
-		p.pq = append(p.pq, 0)
-		// assign all elements in qp to -1 indicating index not set
-		for i := 0; i < len(p.qp); i++ {
-			p.qp[i] = -1
-		}
+		p := NewIndexMinPQ(8)
 		// test insertion with c.in
 		for _, k := range c.in {
 			p.Insert(k[0], k[1])
 			//fmt.Printf("p.pq %v\n", p.pq)
 			//fmt.Printf("p.keys %v\n", p.keys)
 		}
-		assertEqual(t, c.want[0], p.pq[1:])
+		assertEqual(t, c.want[0], p.pq)
 		assertEqual(t, c.want[1], p.qp)
 		assertEqual(t, c.want[2], p.keys)
 	}
 
 }
+
+func TestIndexMinPQContains(t *testing.T) {
+	type testCase struct {
+		in   []tuple2
+		want []bool
+	}
+	cases := []testCase{
+		{
+			[]tuple2{{1, 10}, {2, 12}},
+			[]bool{
+				false, true, true, false,
+				false, false, false, false,
+			},
+		},
+		{
+			[]tuple2{{3, 15}, {0, 9}, {7, 100}, {6, 19}},
+			[]bool{
+				true, false, false, true,
+				false, false, true, true,
+			},
+		},
+	}
+	for _, c := range cases {
+		p := NewIndexMinPQ(8)
+		max := -1
+		for _, tp := range c.in {
+			p.Insert(tp[0], tp[1])
+			if max < tp[0] {
+				max = tp[0]
+			}
+		}
+		for i := 0; i < max+1; i++ {
+			if p.Contains(i) != c.want[i] {
+				t.Logf("p.pq %v\n", p.pq)
+				t.Logf("p.qp %v\n", p.qp)
+				t.Logf("p.keys %v\n", p.keys)
+				t.Fatalf("For %v, no key found at %d\n", c.in, i)
+			}
+		}
+	}
+}
+
+func TestIndexMinPQMinKey(t *testing.T) {
+	type testCase struct {
+		in []tuple2
+		want int
+	}
+	cases := []testCase{
+		{[]tuple2{{5, 13}, {9, 7}}, 7},
+	}
+	for _, c := range cases {
+		p := NewIndexMinPQ(10)
+		for _, tp := range c.in {
+			p.Insert(tp[0], tp[1])
+		}
+		min, _ := p.MinKey()
+		if min != c.want {
+			t.Fatalf("For %v, minkey not %d but %d\n", c.in, c.want, min)
+		}
+	}
+}
+
+func TestIndexMinPQDelMin(t *testing.T) {
+	type testCase struct {
+		in []tuple2
+		want [3][]int
+	}
+	cases := []testCase{
+		{
+			[]tuple2{{2, 1}, {1, 2}},
+			[3][]int{
+				{0, 1, 0, 0, 0, 0, 0, 0, 0},
+				{-1, 2, -1, -1, -1, -1, -1, -1},
+				{0, 2, 1, 0, 0, 0, 0, 0},
+			},
+		},
+		{
+			[]tuple2{{7, 12}, {3, 19}, {0, 3}, {5, 1}},
+			[3][]int{
+				{0, 0, 3, 7, 0, 0, 0, 0, 0},
+				{3, -1, -1, 2, -1, -1, -1, 1},
+				{3, 0, 0, 19, 0, 1, 0, 12},
+			},
+		},
+	}
+	for _, c := range cases {
+		p := NewIndexMinPQ(8)
+		for _, tp := range c.in {
+			p.Insert(tp[0], tp[1])
+		}
+		p.DelMin()
+		assertEqual(t, c.want[0], p.pq)
+		assertEqual(t, c.want[1], p.qp)
+		assertEqual(t, c.want[2], p.keys)
+	}
+
+}
+
+
